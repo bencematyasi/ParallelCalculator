a5b9bf5b0ec44a013304266e71e5e1fa8eb728f2
Bence <sifo996@gmail.com>
2020-10-14 16:51:41 +0200

    Commenting and set timeout for tasks

--- src/com/pcea/Main.java
+++ src/com/pcea/Main.java
@@ -12,54 +12,50 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sequential execution starting...");
-        long startTime = System.currentTimeMillis();
-        long result = Calculator.calculateNumberOfDivisible(0, MAX_NUMBER, DIVISOR);
-        long endTime = System.currentTimeMillis();
-        long elapsedTime = endTime - startTime;
-        System.out.println("Result          : " + result + " calculated under " + elapsedTime + " milliseconds");
+        long startTimeOfSeq = System.currentTimeMillis();
+        long seqRes = Calculator.calculateNumberOfDivisible(0, MAX_NUMBER, DIVISOR);
+        long endTimeOfSeq = System.currentTimeMillis();
+        long elapsedTimeSeq = endTimeOfSeq - startTimeOfSeq;
+        System.out.println("Result          : " + seqRes + " calculated under " + elapsedTimeSeq + " milliseconds");
 
         // Parallel execution
-        System.out.println("Starting parallel execution ....");
-        long timeStartFuture = System.currentTimeMillis();
-
         long resultFuture = 0;
+        System.out.println("Starting parallel execution ....");
+        long startTimeOfFuture = System.currentTimeMillis();
 
-        // Create a new ExecutorService with all available threads to execute and store the Futures
+        // Create a new ExecutorService with all available threads in the machine to execute and store the Futures
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         System.out.println(Runtime.getRuntime().availableProcessors() + " thread(s) available");
-        //
+        //Create list that will store all the FutureTask
         List<FutureTask<Long>> taskList = new ArrayList<>();
-
         for (int i = 1; i < Runtime.getRuntime().availableProcessors() +1 ; i++) {
             var last = MAX_NUMBER/(Runtime.getRuntime().availableProcessors());
             var first= ((last*i)-last)+1;
             if(i == 1){
                 first = 0;
            }
-
+            //Create as many FutureTask as number of threads are available in the machine
             FutureTask<Long> futureTask = new FutureTask<Long>(new CallableCalculator(first, last*i, DIVISOR));
+            //Add this newly created FutureTask to the task list
             taskList.add(futureTask);
+            //Execute the FutureTask
             executor.execute(futureTask);
         }
 
         // Wait until all results are available and combine them at the same time
         for (FutureTask<Long> futureTask : taskList) {
             try {
-                resultFuture += futureTask.get();
-                //TODO:Throw exception if the elapsed time is longer than a given number, will work as a cancellation token
-
-            } catch (InterruptedException | ExecutionException e) {
-                futureTask.cancel(true);
+                resultFuture += futureTask.get(1000,TimeUnit.MILLISECONDS);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
         // Shutdown the ExecutorService
         executor.shutdown();
 
-        long timeEndFuture = System.currentTimeMillis();
-        long timeNeededFuture = timeEndFuture - timeStartFuture;
-        System.out.println("Result (Future): " + resultFuture + " calculated in " + timeNeededFuture + " milliseconds");
+        long endTimeOfFuture = System.currentTimeMillis();
+        long elapsedTimeFuture = endTimeOfFuture - startTimeOfFuture;
+        System.out.println("Result (Future): " + resultFuture + " calculated in " + elapsedTimeFuture + " milliseconds");
 
     }
 
