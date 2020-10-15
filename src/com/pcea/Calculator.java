84b402cfa3ec9d7da2f882c17666bf1192dc7f95
Bence <sifo996@gmail.com>
2020-10-15 14:29:34 +0200

    This is the previous one, forgot to stage the changes

--- src/com/pcea/Calculator.java
+++ src/com/pcea/Calculator.java
@@ -1,5 +1,8 @@
 package com.pcea;
 
+import java.lang.management.ThreadInfo;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Calculator {
     public static long calculateNumberOfDivisible(long first, long last, long divisor) {
         long amountOfNumber = 0;
--- src/com/pcea/Main.java
+++ src/com/pcea/Main.java
@@ -3,60 +3,68 @@ package com.pcea;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     //TODO:Write better comments
 
-    public static final long MAX_NUMBER = 1_000_000_000L;
+    public static final long MAX_NUMBER = 5_000_000_000L;
     private static final long DIVISOR = 3;
 
     public static void main(String[] args) {
         System.out.println("Sequential execution starting...");
-        long startTimeOfSeq = System.currentTimeMillis();
-        long seqRes = Calculator.calculateNumberOfDivisible(0, MAX_NUMBER, DIVISOR);
-        long endTimeOfSeq = System.currentTimeMillis();
-        long elapsedTimeSeq = endTimeOfSeq - startTimeOfSeq;
-        System.out.println("Result          : " + seqRes + " calculated under " + elapsedTimeSeq + " milliseconds");
+        SequentialRun();
+        System.out.println("----------------------------------------");
+        System.out.println("Parallel execution is starting...");
+        ParallelRun();
+    }
+    private static void SequentialRun(){
+        double start = System.currentTimeMillis();
+        long result = Calculator.calculateNumberOfDivisible(1, MAX_NUMBER, DIVISOR);
+        double stop = System.currentTimeMillis();
+        double seconds = (stop-start)/ 1000.000d;
+        System.out.println("Result: " + result + " calculated under " + seconds + " seconds");
+    }
+    private static void ParallelRun(){
+        AtomicLong result = new AtomicLong();
+        double start = System.currentTimeMillis();
 
-        // Parallel execution
-        long resultFuture = 0;
-        System.out.println("Starting parallel execution ....");
-        long startTimeOfFuture = System.currentTimeMillis();
-
-        // Create a new ExecutorService with all available threads in the machine to execute and store the Futures
+        // Create a new ExecutorService and set all available threads in the machine to be used in its ThreadPool
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        System.out.println(Runtime.getRuntime().availableProcessors() + " thread(s) available");
         //Create list that will store all the FutureTask
         List<FutureTask<Long>> taskList = new ArrayList<>();
         for (int i = 1; i < Runtime.getRuntime().availableProcessors() +1 ; i++) {
-            var last = MAX_NUMBER/(Runtime.getRuntime().availableProcessors());
-            var first= ((last*i)-last)+1;
-            if(i == 1){
-                first = 0;
-           }
+            var fraction = MAX_NUMBER/(Runtime.getRuntime().availableProcessors());
+            var first= ((fraction*i)-fraction)+1;
+            var last = fraction * i;
+
             //Create as many FutureTask as number of threads are available in the machine
-            FutureTask<Long> futureTask = new FutureTask<Long>(new CallableCalculator(first, last*i, DIVISOR));
+            FutureTask<Long> futureTask = new FutureTask<Long>(new CallableCalculator(first, last, DIVISOR));
             //Add this newly created FutureTask to the task list
             taskList.add(futureTask);
             //Execute the FutureTask
-            executor.execute(futureTask);
+            //executor.execute(futureTask);
         }
+        /* Execute/start each task, the order of execution does not matter*/
+        taskList.parallelStream().forEach(task -> executor.execute(task));
 
-        // Wait until all results are available and combine them at the same time
-        for (FutureTask<Long> futureTask : taskList) {
+        /*Wait for all tasks to be completed and add the tasks' return value/result to the AtomicLong result variable.
+        The order of addition does not matter, but to receive all the results the AtomicLong is required to have
+        Task will be canceled after 5 seconds of runtime*/
+        taskList.parallelStream().forEach(task ->{
             try {
-                resultFuture += futureTask.get(1000,TimeUnit.MILLISECONDS);
-            } catch (Exception e) {
+                result.addAndGet(task.get(5000, TimeUnit.MILLISECONDS));
+            } catch (CancellationException | InterruptedException | TimeoutException | ExecutionException e) {
+                task.cancel(true);
                 e.printStackTrace();
             }
-        }
+        });
         // Shutdown the ExecutorService
         executor.shutdown();
 
-        long endTimeOfFuture = System.currentTimeMillis();
-        long elapsedTimeFuture = endTimeOfFuture - startTimeOfFuture;
-        System.out.println("Result (Future): " + resultFuture + " calculated in " + elapsedTimeFuture + " milliseconds");
+        double stop = System.currentTimeMillis();
+        double seconds = (stop - start)/1000.000d;
+        System.out.println("Result (Future): " + result + " calculated in " + seconds + " seconds");
 
     }
-
 }
